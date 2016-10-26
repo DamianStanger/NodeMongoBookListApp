60bb34db3dde058111f73f9d95ce66001292dfce
damo <damo@localhost.localdomain>
2016-10-26 18:39:10 +0100

    added bootstrap theme

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 app = express();
+
 var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 app.get('/', function (req, res) {
     res.send("foo foo foo foo foobar");
 });
