1254bf469ce87e4220178416a67a92a1ba475b0d
damo <damo@localhost.localdomain>
2016-10-28 22:47:25 +0100

    added gulp files for jshint and jscs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require("express");
-app = express();
+var express = require('express');
+var app = express();
 
 var port = 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 app.get('/', function (req, res) {
-    res.send("foo foo foo foo foobar");
+    res.send('foo foo foo foo foobar');
 });
 app.listen(port, function (err) {
-    console.log("listening on port:" + port);
-})
-console.log("foobar");
+    console.log('listening on port:' + port);
+});
+console.log('foobar');
