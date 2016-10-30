581d6d69e6c618dc96cf8edabbb59e30cfdbded0
damo <damo@localhost.localdomain>
2016-10-30 14:42:00 +0000

    Use guld inject with wiredep to bring in all the js and css dependencies into the index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
+
 var jsFiles =  ['*.js', 'src/**/*.js'];
 
 gulp.task ('style', function  () {
@@ -12,4 +13,24 @@ gulp.task ('style', function  () {
         }))
         .pipe(jscs())
         .pipe(jscs.reporter());
+});
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var options = {
+        dowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+    var injectSrc = gulp.src(['./public/css/*.css','./public/js/*.js'], {read: false});
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
 });
